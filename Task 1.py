5cacc1fb6b4eb79699a90befed9b12c8396b6b17
ashishharidas <ashish101111@gmail.com>
2025-06-17 19:59:06 +0530

    Update Task 1.py

--- Task 1.py
+++ Task 1.py
@@ -2,6 +2,6 @@
 a=int (input("Enter the number: ") )
 
 if a%2==0:
-    print(f"{a} is a Even number")
+    print(f"{a} is an Even number")
 else:    
-    print(f"{a} is a Odd number")
+    print(f"{a} is an Odd number")
